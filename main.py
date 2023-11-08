6775e26a294b9e0b19d38e6a5eab8eb84cc6d333
FabianGnatzig <fabiangnatzig@gmx.de>
2023-11-08 17:40:08 +0100

    init camera

---
+++ main.py
@@ -0,0 +1,12 @@
+from time import sleep
+from picamera2 import Picamera2, Preview
+
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration()
+print(camera_config)
+picam2.configure(camera_config)
+picam2.start()
+picam2.start_preview(Preview.QT)
+sleep(2)
+picam2.capture_file("test.jpg")
+print("HelloWorld")
