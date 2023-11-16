9afbc53908e8011d4085bf77294d178f1ee343c8
FabianGnatzig <fabiangnatzig@gmx.de>
2023-11-16 19:56:37 +0100

    Added class camera

--- KameraTryout/.idea/fileTemplates/internal/Python Script.py
+++ KameraTryout/.idea/fileTemplates/internal/Python Script.py
@@ -1,12 +1,8 @@
 """
+###########################################################
 Created for project "Personenverfolger" for DHBW Heidenheim
 
 Created by Fabian Gnatzig in 2023
 Contact: fabiangnatzig@gmx.de
+###########################################################
 """
-from color_camera import ColorCamera
-
-if __name__ == '__main__':
-
-    camera = ColorCamera()
-    camera.run()
