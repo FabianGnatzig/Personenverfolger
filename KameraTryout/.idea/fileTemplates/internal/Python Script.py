2556b3ddaf46bb3514e6e0892bcb33dfe959a124
Fabian Gnatzig <93179342+FabianGnatzig@users.noreply.github.com>
2023-11-30 09:28:47 +0100

    Delete KameraTryout/.idea directory

--- KameraTryout/.idea/fileTemplates/internal/Python Script.py
+++
@@ -1,8 +0,0 @@
-"""
-###########################################################
-Created for project "Personenverfolger" for DHBW Heidenheim
-
-Created by Fabian Gnatzig in 2023
-Contact: fabiangnatzig@gmx.de
-###########################################################
-"""
