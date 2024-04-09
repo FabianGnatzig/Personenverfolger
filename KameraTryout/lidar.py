1c12467a03cd3fe8983cfc4f92c6c617bde9d59a
FabianGnatzig <fabiangnatzig@gmx.de>
2024-04-09 17:32:29 +0200

    Created angled measurements from lidar

--- KameraTryout/lidar.py
+++ KameraTryout/lidar.py
@@ -3,6 +3,7 @@ Created by Fabian Gnatzig in 2024
 Contact: fabiangnatzig@gmx.de
 """
 import logging
+import time
 
 from rplidar import RPLidar
 
@@ -16,6 +17,10 @@ class Lidar:
 
     def __init__(self, port: str):
         self._port = port
+        self._measurements = []
+        self._angle_measurements = []
+        self._run = True
+        self._ready = False
         try:
             self._sensor = RPLidar(port)
         except Exception as ex:
@@ -26,15 +31,48 @@ class Lidar:
         Test method.
         """
         log.info(f"{self._sensor.get_info()}, {self._sensor.get_health()}")
+        self._ready = True
 
-        for i, scans in enumerate(self._sensor.iter_scans()):
-            for measurement in scans:
-                _, angle, distance = measurement
-                log.info(f"Angle: {angle}; Distance: {distance}")
+        for i, scan in enumerate(self._sensor.iter_scans()):
+            self._measurements = scan
+            self.get_angle_measurements(scan)
 
-            if i > 10:
+            if not self._run:
                 break
 
         self._sensor.stop()
         self._sensor.stop_motor()
         self._sensor.disconnect()
+
+    def get_angle_measurements(self, measurements):
+        angle_measurements = []
+        for _, angle, distance in measurements:
+            if 45 < angle < 315:
+                continue
+
+            angle_measurements.append((angle, distance))
+        self._angle_measurements = angle_measurements
+
+    @property
+    def measurements(self):
+        return self._measurements
+
+    @property
+    def run(self):
+        return self._run
+
+    @run.setter
+    def run(self, value: bool):
+        self._run = value
+
+    @property
+    def ready(self):
+        return self._ready
+
+    @ready.setter
+    def ready(self, value: bool):
+        self._ready = value
+
+    @property
+    def angle_measurements(self):
+        return self._angle_measurements
--- KameraTryout/main.py
+++ KameraTryout/main.py
@@ -5,6 +5,8 @@ Created by Fabian Gnatzig in 2023
 Contact: fabiangnatzig@gmx.de
 """
 import logging
+import time
+from threading import Thread
 
 from color_camera import ColorCamera
 from custom_formatter import CustomFormatter
@@ -25,7 +27,24 @@ def lidar():
     Test method for LIDAR
     """
     lidar_sensor = Lidar(PORT_NAME)
-    lidar_sensor.test_measurement()
+
+    lidar_thread = Thread(target=lidar_sensor.test_measurement)
+    lidar_thread.start()
+
+    i = 0
+
+    while i < 10:
+        log.info(f"Number of measurements {len(lidar_sensor.measurements)}")
+        log.info(f"Number of angled measurements {len(lidar_sensor.angle_measurements)}")
+
+        for andist in lidar_sensor.angle_measurements:
+            log.info(andist)
+
+        time.sleep(0.5)
+        i += 1
+
+    lidar_sensor.run = False
+    lidar_thread.join()
 
 
 def camera():
