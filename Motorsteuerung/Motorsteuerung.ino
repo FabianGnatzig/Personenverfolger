2e0aca87eb31fd1143494337addf2b90f1766d9b
Fabian Gnatzig <93179342+FabianGnatzig@users.noreply.github.com>
2024-07-10 10:49:56 +0200

    Update Motorsteuerung.ino

--- Motorsteuerung/Motorsteuerung.ino
+++ Motorsteuerung/Motorsteuerung.ino
@@ -6,9 +6,16 @@ int MotorB_Enable = 6; // Pwm Pin for motor B
 int MotorB_in3 = 5;
 int MotorB_in4 = 4;
 
-// the setup function runs once when you press reset or power the board
+// Pin definitions for receiving signals
+int SpeedPin1 = 2;
+int SpeedPin2 = 3;
+int DirectionPin1 = 10;
+int DirectionPin2 = 11;
+int DirectionPin3 = 12;
+
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // outputs
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(MotorA_Enable, OUTPUT);
   pinMode(MotorA_in1, OUTPUT);
@@ -16,127 +23,152 @@ void setup() {
   pinMode(MotorB_Enable, OUTPUT);
   pinMode(MotorB_in3, OUTPUT);
   pinMode(MotorB_in4, OUTPUT);
+  
+  // inputs
+  pinMode(SpeedPin1, INPUT);
+  pinMode(SpeedPin2, INPUT);
+  pinMode(DirectionPin1, INPUT);
+  pinMode(DirectionPin2, INPUT);
+  pinMode(DirectionPin3, INPUT);
 }
-
-// serial communication?
-// Serial.begin(9600);
 
 
 // the loop function runs over and over again forever
 void loop() {
 
-  driveForward(255); // Fullthrottle
+  int speed = getSpeed();
+  int direction = getDirection();
+   switch (direction) {
+    case 0b000:
+      stopMotors();
+      break;
+    case 0b001:
+      driveForward(speed);
+      break;
+    case 0b010:
+      driveBackward(speed);
+      break;
+    case 0b011:
+      turnHardLeft(speed);
+      break;
+    case 0b100:
+      turnHardRight(speed);
+      break;
+    case 0b101:
+      turnSoftLeft(speed);
+      break;
+    case 0b110:
+      turnSoftRight(speed);
+      break;
+    default:
+      stopMotors();
+      break;
+   }
+
+}
+
+/* Test Funktionen für den Loop:
+ driveForward(150); // Fullthrottle
   delay(4000);       // wait for two second
 
-  driveBackward(255);
-  delay(4000);
-
-  turnHardLeft(255);
-  delay(4000);
-
-  turnHardRight(255);
-  delay(4000);
-
-  turnSoftLeft(255);
-  delay(4000);
-
-  turnSoftRight(255);
+  turnSoftLeft(150);
   delay(4000);
 
   stopMotors();
   delay(4000);
 
-}
+  turnHardLeft(150);
+  delay(4000);
+
+  driveBackward(150);
+  delay(4000);
+
+  turnHardRight(150);
+  delay(4000);
+ 
+  turnSoftRight(150);
+  delay(4000);
+  */
 
 // MOTOR A -> left tire, MOTOR B -> right tire
+
+int getSpeed() {
+  int speed = 0;
+  if (digitalRead(SpeedPin1) == HIGH) speed += 85;
+  if (digitalRead(SpeedPin2) == HIGH) speed += 170;
+  return speed;
+}
+
+int getDirection() {
+  int direction = 0;
+  if (digitalRead(DirectionPin1) == HIGH) direction += 0b001;
+  if (digitalRead(DirectionPin2) == HIGH) direction += 0b010;
+  if (digitalRead(DirectionPin3) == HIGH) direction += 0b100;
+  return direction;
+}
+
 
 void driveForward(int speed){
   analogWrite(MotorA_Enable, speed);
   digitalWrite(MotorA_in1,HIGH);
   digitalWrite(MotorA_in2,LOW);
-
   analogWrite(MotorB_Enable, speed);
   digitalWrite(MotorB_in3,HIGH);
   digitalWrite(MotorB_in4,LOW);
 }
 
-/*void Turn_Hard_left(){
-  // Motor A -> LINKS, Motor B -> Rechts
-  MotorA_Backward();
-  MotorB_Forward();
-}
-
-void Turn_Hard_right(){
-MotorA_Forward();
-MotorB_Backward();
-} */
-
 void driveBackward(int speed){
-
   analogWrite(MotorA_Enable, speed);
   digitalWrite(MotorA_in1, LOW);
   digitalWrite(MotorA_in2, HIGH);
-
   analogWrite(MotorB_Enable, speed);
   digitalWrite(MotorB_in3, LOW);
   digitalWrite(MotorB_in4, HIGH);  
 }
 
 void turnHardLeft(int speed){
-
   analogWrite(MotorA_Enable, speed);
   digitalWrite(MotorA_in1, LOW);
   digitalWrite(MotorA_in2, HIGH);
-  
-
   analogWrite(MotorB_Enable, speed);
   digitalWrite(MotorB_in3, HIGH);
   digitalWrite(MotorB_in4, LOW);
 }
 
 void turnHardRight(int speed){
-
   analogWrite(MotorA_Enable, speed);
   digitalWrite(MotorA_in1, HIGH);
   digitalWrite(MotorA_in2, LOW);
-  
   analogWrite(MotorB_Enable, speed);
   digitalWrite(MotorB_in3, LOW);
   digitalWrite(MotorB_in4, HIGH);
 }
 
 void turnSoftLeft(int speed) {
-  analogWrite(MotorA_Enable, speed * 0.8); // Reduced speed for left tire for soft turn
-  digitalWrite(MotorA_in1, LOW);
-  digitalWrite(MotorA_in2, HIGH);
-
+  analogWrite(MotorA_Enable, speed * 0.7); // Reduced speed for left tire for soft turn
+  digitalWrite(MotorA_in1, HIGH);
+  digitalWrite(MotorA_in2, LOW);
   analogWrite(MotorB_Enable, speed);
   digitalWrite(MotorB_in3, HIGH);
   digitalWrite(MotorB_in4, LOW);
 }
   
 
-  void turnSoftRight(int speed) {
+void turnSoftRight(int speed) {
   analogWrite(MotorA_Enable, speed);
   digitalWrite(MotorA_in1, LOW);
   digitalWrite(MotorA_in2, HIGH);
-
-  analogWrite(MotorB_Enable, speed * 0.8);  // Reduced speed for right tire for soft turn
+  analogWrite(MotorB_Enable, speed * 0.7);  // Reduced speed for right tire for soft turn
   digitalWrite(MotorB_in3, LOW);
   digitalWrite(MotorB_in4, HIGH);
   }
   
 
 void stopMotors(){
-
   analogWrite(MotorA_Enable, 0);
   digitalWrite(MotorA_in1, LOW);
   digitalWrite(MotorA_in2, LOW);
-  
   analogWrite(MotorB_Enable, 0);
   digitalWrite(MotorB_in3, LOW);
   digitalWrite(MotorB_in4, LOW);
-  
-  
 }
-
